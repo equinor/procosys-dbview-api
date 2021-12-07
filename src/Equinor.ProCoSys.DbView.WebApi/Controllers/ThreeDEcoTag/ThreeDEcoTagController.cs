e7e127076501d24ceeb08b53d8d1df454c150398
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-12-07 13:09:01 +0100

    minor
    (cherry picked from commit 2b5807b0cc3fda39b3d6e811d8c2077c4990ea03)

--- src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/ThreeDEcoTagController.cs
+++ src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/ThreeDEcoTagController.cs
@@ -15,7 +15,7 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
         /// <summary>
         /// POC! Count all Tags available for 3D Ecosystems POC!
         /// </summary>
-        /// <param name="installationCode">Installation code, representing the plant(I.e JSV, GRA, TROA etc...)</param>
+        /// <param name="installationCode">Installation code, representing the plant (I.e JSV, GRA, TROA etc...)</param>
         /// <remarks>This is a POC. Can be changed or removed at any time</remarks>
         /// <response code="200">OK</response>
         [SwaggerOperation(Tags = new[] { "3DEcosystem" })]
@@ -29,7 +29,7 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
         /// <summary>
         /// POC! Get page of Tags available for 3D Ecosystems POC!
         /// </summary>
-        /// <param name="installationCode">Installation code, representing the plant(I.e JSV, GRA, TROA etc...)</param>
+        /// <param name="installationCode">Installation code, representing the plant (I.e JSV, GRA, TROA etc...)</param>
         /// <param name="currentPage">Current page to get. Default is 0 (first page)</param>
         /// <param name="itemsPerPage">Number of items pr page. Default is 100000</param>
         /// <param name="max">Max records to return. For swagger testing to avoid freezing swagger. Default = 0 will return all</param>
