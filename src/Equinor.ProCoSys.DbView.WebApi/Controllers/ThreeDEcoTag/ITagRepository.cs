16636346dd155893aaba3e2610f8b1cf3940162f
Jan Inge Dalsbø <JDALSB@equinor.com>
2021-12-13 14:57:55 +0100

    Rermoved enpoint api/DbView/3DEcosystem/Count

--- src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/ITagRepository.cs
+++ src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/ITagRepository.cs
@@ -2,7 +2,6 @@
 {
     public interface ITagRepository
     {
-        TagMaxAvailableModel GetMaxAvailable(string installationCode);
         TagModel GetPage(string installationCode, int currentPage, int itemsPerPage, int takeMax = 0);
     }
 }
