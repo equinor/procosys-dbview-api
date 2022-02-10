d3cb5dc88279f6e649f017c12ef983f798eb5a35
Jan Inge Dalsbø <JDALSB@equinor.com>
2022-02-10 11:33:23 +0100

    Updates in select for linetags

--- src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/TagRepository.cs
+++ src/Equinor.ProCoSys.DbView.WebApi/Controllers/ThreeDEcoTag/TagRepository.cs
@@ -17,6 +17,7 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
         private static readonly string s_tagNo = "TagNo";
         private static readonly string s_project = "Project";
         private static readonly string s_punchCount = "PunchCount";
+        private static readonly string s_commPkg_Id = "CommPkg_Id";
         private static readonly string s_commPkgNo = "CommPkgNo";
         private static readonly string s_commPkgDesc = "CommPkgDesc";
         private static readonly string s_mcPkgNo = "McPkgNo";
@@ -30,35 +31,39 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
 
         private static readonly string s_instCodeToken = "[INSTCODE]";
 
-
+        // Don't use AS-statemens on ID-columns used in order by clause
         private static readonly string s_tagQuery =
-            $@"SELECT T.TAGNO AS ""{s_tagNo}"",
-                   PR.NAME AS ""{s_project}"",
+            $@"SELECT t.tagno AS ""{s_tagNo}"",
+                   t.tag_id,
+                   pr.name AS ""{s_project}"",
+                   PR.PROJECT_ID,
                    MC.MCPKGNO AS ""{s_mcPkgNo}"",
                    MC.DESCRIPTION AS ""{s_mcPkgDesc}"",
+                   C.COMMPKG_ID,
                    C.COMMPKGNO AS ""{s_commPkgNo}"",
                    C.DESCRIPTION AS ""{s_commPkgDesc}"",
                    PRI.CODE AS ""{s_priority}"",
                    PH.CODE AS ""{s_phase}"",
                    RES.CODE AS ""{s_responsible}"",
-                   STAT.CODE AS ""{s_status}"",
+                   RES.RESPONSIBLE_ID,
+                   stat.CODE AS ""{s_status}"",
                    (SELECT COUNT(1)
-                      FROM PROCOSYS.PUNCHLISTITEM PI
+                      FROM procosys.punchlistitem pi
                      WHERE     PI.TAGCHECK_ID = TC.TAGCHECK_ID
-                           AND PI.ISVOIDED = 'N'
-                           AND ((PI.CLEAREDAT IS NULL) OR (PI.REJECTEDAT IS NOT NULL)))
+                           AND pi.isvoided = 'N'
+                           AND((PI.CLEAREDAT IS NULL) OR(PI.REJECTEDAT IS NOT NULL)))
                       AS ""{s_punchCount}"",
-                      'na' AS ""{s_rfcc}"",
-                      'na' AS ""{s_rfoc}""
-              FROM PROCOSYS.TAG T
-                   INNER JOIN PROCOSYS.PROJECT PR
-                      ON     PR.PROJECT_ID = T.PROJECT_ID
-                         AND PR.ISCLOSED = 'N'
-                         AND (PR.NAME LIKE '_.%')
-                   INNER JOIN PROCOSYS.LIBRARY FAC ON FAC.LIBRARY_ID = T.INSTALLATION_ID
-                   INNER JOIN PROCOSYS.LIBRARY LIB_REGISTER
+                    'na' AS ""{s_rfcc}"",
+                    'na' AS ""{s_rfoc}""
+              FROM procosys.tag t
+                   INNER JOIN procosys.project pr
+                      ON pr.PROJECT_ID = t.project_id
+                         AND pr.isclosed = 'N'
+                         AND (pr.name LIKE '_.%')
+                   INNER JOIN procosys.library fac ON FAC.LIBRARY_ID = t.installation_id
+                   INNER JOIN procosys.library lib_register
                       ON LIB_REGISTER.LIBRARY_ID = T.REGISTER_ID
-                         AND LIB_REGISTER.CODE IN ('INSTRUMENT_FIELD',
+                         AND LIB_REGISTER.code IN ('INSTRUMENT_FIELD',
                                                    'MAIN_EQUIPMENT',
                                                    'ELECTRICAL_FIELD',
                                                    'FIRE_AND_GAS_FIELD',
@@ -70,21 +75,70 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
                                                    'PIPE_SUPPORT',
                                                    'DUCTING',
                                                    'PIPELINE',
-                                                   'HOSE_ASSEMBLY',
-                                                   'LINE')
-                   LEFT OUTER JOIN PROCOSYS.MCPKG MC ON MC.MCPKG_ID = T.MCPKG_ID
-                   LEFT OUTER JOIN PROCOSYS.COMMPKG C ON C.COMMPKG_ID = MC.COMMPKG_ID
-                   LEFT OUTER JOIN PROCOSYS.LIBRARY PRI
-                      ON PRI.LIBRARY_ID = C.COMMPRIORITY_ID
-                   LEFT OUTER JOIN PROCOSYS.LIBRARY PH ON PH.LIBRARY_ID = C.COMMPHASE_ID
+                                                   'HOSE_ASSEMBLY')
+                   LEFT OUTER JOIN procosys.mcpkg mc ON MC.MCPKG_ID = t.mcpkg_id
+                   LEFT OUTER JOIN procosys.commpkg c ON C.COMMPKG_ID = mc.commpkg_id
                    LEFT OUTER JOIN PROCOSYS.TAGFORMULARTYPE TFT ON TFT.TAG_ID = T.TAG_ID
                    LEFT OUTER JOIN PROCOSYS.TAGCHECK TC
                       ON TC.TAGFORMULARTYPE_ID = TFT.TAGFORMULARTYPE_ID
-                   LEFT OUTER JOIN PROCOSYS.RESPONSIBLE RES
+                   LEFT OUTER JOIN procosys.responsible res
                       ON RES.RESPONSIBLE_ID = TC.RESPONSIBLE_ID
-                   LEFT OUTER JOIN PROCOSYS.LIBRARY STAT
-                      ON STAT.LIBRARY_ID = TC.STATUS_ID
-             WHERE FAC.CODE = '{s_instCodeToken}' ";
+                   LEFT OUTER JOIN procosys.library pri
+                      ON PRI.LIBRARY_ID = C.COMMPRIORITY_ID
+                   LEFT OUTER JOIN procosys.library ph ON PH.LIBRARY_ID = C.COMMPHASE_ID
+                   LEFT OUTER JOIN procosys.library stat
+                      ON stat.LIBRARY_ID = TC.STATUS_ID
+                WHERE FAC.CODE = '{s_instCodeToken}'
+            UNION ALL
+            SELECT DISTINCT
+                   t.tagno AS ""{ s_tagNo}"",
+                   t.tag_id,
+                   pr.name AS ""{s_project}"",
+                   PR.PROJECT_ID,
+                   MC.MCPKGNO AS ""{s_mcPkgNo}"",
+                   MC.DESCRIPTION AS ""{s_mcPkgDesc}"",
+                   C.COMMPKG_ID,
+                   C.COMMPKGNO AS ""{s_commPkgNo}"",
+                   C.DESCRIPTION AS ""{s_commPkgDesc}"",
+                   PRI.CODE AS ""{s_priority}"",
+                   PH.CODE AS ""{s_phase}"",
+                   RES.CODE AS ""{s_responsible}"",
+                   RES.RESPONSIBLE_ID,
+                   stat.CODE AS ""{s_status}"",
+                   (SELECT COUNT (1)
+                      FROM procosys.punchlistitem pi
+                     WHERE     PI.TAGCHECK_ID = TC.TAGCHECK_ID
+                           AND pi.isvoided = 'N'
+                           AND ((PI.CLEAREDAT IS NULL) OR (PI.REJECTEDAT IS NOT NULL)))
+                      AS ""{s_punchCount}"",
+                    'na' AS ""{s_rfcc}"",
+                    'na' AS ""{s_rfoc}""
+              FROM procosys.tag t
+                   INNER JOIN procosys.project pr
+                      ON pr.PROJECT_ID = t.project_id
+                         AND pr.isclosed = 'N'
+                         AND (pr.name LIKE '_.%')
+                   INNER JOIN procosys.library fac ON FAC.LIBRARY_ID = t.installation_id
+                   INNER JOIN procosys.library lib_register
+                      ON LIB_REGISTER.LIBRARY_ID = T.REGISTER_ID
+                         AND LIB_REGISTER.code = 'LINE'
+                   LEFT OUTER JOIN procosys.pipingspool ps ON ps.tag_id = t.tag_id
+                   LEFT OUTER JOIN procosys.pipingrevision pr
+                      ON PR.PIPINGREVISION_ID = PS.PIPINGREVISION_ID
+                   LEFT OUTER JOIN procosys.pipetest pt
+                      ON pt.PIPINGREVISION_ID = PR.PIPINGREVISION_ID
+                   LEFT OUTER JOIN PROCOSYS.TAGCHECK TC
+                      ON TC.TAGCHECK_ID = pt.TAGCHECK_ID
+                   LEFT OUTER JOIN procosys.mcpkg mc ON MC.MCPKG_ID = pr.mcpkg_id
+                   LEFT OUTER JOIN procosys.commpkg c ON C.COMMPKG_ID = mc.commpkg_id
+                   LEFT OUTER JOIN procosys.responsible res
+                      ON RES.RESPONSIBLE_ID = TC.RESPONSIBLE_ID
+                   LEFT OUTER JOIN procosys.library pri
+                      ON PRI.LIBRARY_ID = C.COMMPRIORITY_ID
+                   LEFT OUTER JOIN procosys.library ph ON PH.LIBRARY_ID = C.COMMPHASE_ID
+                   LEFT OUTER JOIN procosys.library stat
+                      ON stat.LIBRARY_ID = TC.STATUS_ID
+                WHERE FAC.CODE = '{s_instCodeToken}'";
 
         public TagRepository(
             IConfiguration configuration,
@@ -125,7 +179,8 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
                 s_rfcc,
                 s_rfoc,
                 s_responsible,
-                s_status
+                s_status,
+                s_commPkg_Id
             };
 
         private static IEnumerable<IEnumerable<object>> GeTagPropertiesInstances(DataTable dataTable)
@@ -145,7 +200,9 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
                     row[s_rfcc] as string,
                     row[s_rfoc] as string,
                     row[s_responsible] as string,
-                    row[s_status] as string
+                    row[s_status] as string,
+                    row[s_commPkg_Id] == DBNull.Value
+                        ? null : Convert.ToInt32(row[s_commPkg_Id]),
                 }
                 ).ToList();
             return instances;
@@ -162,10 +219,10 @@ namespace Equinor.ProCoSys.DbView.WebApi.Controllers.ThreeDEcoTag
                 var message = $"Getting {itemsPerPage} records at page {currentPage} for 3D Ecosystems for installation code {installationCode}";
                 strSql += 
                     $@"
-                        ORDER BY T.TAG_ID,
-                        PR.PROJECT_ID,
-                        C.COMMPKG_id,
-                        RES.RESPONSIBLE_ID
+                        ORDER BY TAG_ID,
+                        PROJECT_ID,
+                        COMMPKG_ID,
+                        RESPONSIBLE_ID
                     OFFSET {currentPage * itemsPerPage} ROWS FETCH NEXT {itemsPerPage} ROWS ONLY";
                 _logger.LogInformation(message);
                 
